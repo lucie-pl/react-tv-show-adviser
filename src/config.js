0370ce5091c89cc9ad8cfc414a24ae49855bd999
Lucie Plantier <lucie.plantier@live.fr>
2024-02-28 16:12:58 -0500

    Storing specific path for fetch

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY_PARAM = 'api_key=872c315f3aeed4ba61e68edf074b811e ';
+const BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/original';
+const BACKDROP_MINIATURE_BASE_URL = 'https://image.tmdb.org/t/p/w300/';
+
+export {
+  BASE_URL,
+  API_KEY_PARAM,
+  BACKDROP_BASE_URL,
+  BACKDROP_MINIATURE_BASE_URL,
+};
