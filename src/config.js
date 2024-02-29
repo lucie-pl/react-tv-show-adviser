74acd57f3139e08e6f5208a75eef0b2f10c54e93
Lucie Plantier <lucie.plantier@live.fr>
2024-02-28 22:42:20 -0500

    Configuring .env with API key
    Adding a configuration file to store our API key and add it to our
    .gitignore file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_KEY_PARAM = 'api_key=872c315f3aeed4ba61e68edf074b811e ';
+const API_KEY_PARAM = `api_key=${process.env.REACT_APP_TMDB_API_KEY}`;
 const BACKDROP_BASE_URL = 'https://image.tmdb.org/t/p/original';
 const BACKDROP_MINIATURE_BASE_URL = 'https://image.tmdb.org/t/p/w300/';
 
